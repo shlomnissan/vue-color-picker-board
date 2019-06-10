822e427d7b843696d40d805f8bff853981b82722
Shlomi Nissan <shlomi@betamark.com>
2019-06-10 09:21:53 -0700

    Add missing test cases

--- tests/ColorPickerBoard.spec.js
+++ tests/ColorPickerBoard.spec.js
@@ -1,9 +1,44 @@
-import { mount } from '@vue/test-utils';
+import { shallowMount } from '@vue/test-utils';
 import ColorPickerBoard from '../src/ColorPickerBoard.vue';
 
 describe('ColorPickerBoard', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(ColorPickerBoard)
-    expect(wrapper.isVueInstance()).toBeTruthy()
-  })
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(ColorPickerBoard);
+  });
+
+  it("is vue instance", () => {
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  it("renders correctly", () => {
+    expect(wrapper.vm.$el).toMatchSnapshot();
+  });
+
+  describe(":props", () => {
+    it(":width", () => {
+      wrapper.setProps({ width: 100 });
+      expect(wrapper.find("#canvas").attributes('width')).toEqual("100");
+    });
+
+    it(":height", () => {
+      wrapper.setProps({ height: 100 });
+      expect(wrapper.find("#canvas").attributes('height')).toEqual("100");
+    });
+
+    it(":defaultColor", () => {
+      const wrapper = shallowMount(ColorPickerBoard, {
+        propsData: {defaultColor: "#FF00AA"}
+      });
+      expect(wrapper.find("#canvas").element.style.backgroundColor).toEqual("rgb(255, 0, 170)");
+    });
+  });
+
+  describe("@events", () => {
+    it("@onSelection", () => {
+      wrapper.find('#canvas').trigger('click');
+      expect(wrapper.emitted()["onSelection"]).toBeTruthy();
+    });
+  });
 });
